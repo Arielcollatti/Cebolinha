d0343154b85358f21d8149d6c04ff560f8a7150f
arielcollatti <colatti2017@gmail.com>
2023-05-15 12:10:39 -0300

    Exercicio realizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("--- Cebolinha ---\n");
+string digite, fraseCebolinha;
+
+Console.Write("digite uma frase:  ");
+digite = Console.ReadLine()!;
+
+fraseCebolinha = digite
+     .Replace("r","l")
+     .Replace("R","L");
+
+Console.WriteLine($"\nA frase digitada pelo cebolinha é: {fraseCebolinha}");
